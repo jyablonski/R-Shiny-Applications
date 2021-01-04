f586fc6cc802c52204146284bafb5207ed4f9dbc
jyablonski <jyablonski9@gmail.com>
2021-01-04 12:53:48 -0800

    Update app.R

--- NBA/app.R
+++ NBA/app.R
@@ -1,15 +1,14 @@
 source('global.R')
-# source('shot_loc_main.R')
 source('content/body_standings.R')
 source('content/body_recent.R')
 source('content/body_team_plots.R')
 source('content/body_about.R')
 source('content/body_schedule.R')
-# source('content/body_shot_location.R')
 
 
 ui <- fluidPage(
-  navbarPage('2019-2020 NBA Season Dashboard',
+  tags$head(includeHTML(("google-analytics.html"))),
+  navbarPage('2020-2021 NBA Season Dashboard',
              tabPanel("Overview", dashboardPage(title = "Overview",
                                                 header = dashboardHeader(disable = TRUE),
                                                 sidebar = dashboardSidebar(disable = TRUE),
@@ -26,11 +25,6 @@ ui <- fluidPage(
                                                 header = dashboardHeader(disable = TRUE),
                                                 sidebar = dashboardSidebar(disable = TRUE),
                                                 body = body_schedule)),
-             # tabPanel("Shot Location App", dashboardPage(title = "Shot Location App",
-             #                                    header = dashboardHeader(disable = TRUE),
-             #                                    sidebar = dashboardSidebar(disable = TRUE),
-             #                                    body = body_shot_location)),
-             
              tabPanel("About", dashboardPage(title = "About",
                                              header = dashboardHeader(disable = TRUE),
                                              sidebar = dashboardSidebar(disable = TRUE),
@@ -41,7 +35,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   output$today_date <- renderText({
-    paste0('Data Updated as of ', today)
+    paste0('Data Updated as of ', updated_date)
     
   })
   
@@ -56,6 +50,8 @@ server <- function(input, output, session) {
                                                                     lengthChange = FALSE, info = FALSE,
                                                                     paging = FALSE))
   
+  output$transactions_table <- DT::renderDataTable(transactions)
+  
   output$schedule_table <- DT::renderDataTable(schedule_main, rownames = FALSE,
                                                options = list(pageLength = 20))
 
@@ -67,7 +63,7 @@ server <- function(input, output, session) {
   
   output$recent_games_played <- renderValueBox({
     valueBox(
-      value = recent_Bans$`Number of Games`, "Game Played", icon = icon("list"), color = "purple"
+      value = recent_Bans$`Number of Games`, "Games Played", icon = icon("list"), color = "purple"
     )
   })
   
@@ -84,22 +80,24 @@ server <- function(input, output, session) {
   })
 
   output$top_15 <- DT::renderDataTable(datatable(top_15_yesterday, rownames = FALSE,
-                                       options = list(searching = FALSE, pageLength = 15, 
-                                                      lengthChange = FALSE, info = FALSE, paging = FALSE)) %>%
+                                                 options = list(searching = FALSE, pageLength = 15, 
+                                                                lengthChange = FALSE, info = FALSE, paging = FALSE)) %>%
                                          formatCurrency(6, currency = "$", interval = 3, mark = ",", digits = 0) %>%
                                          formatStyle(c(1:dim(top_15_yesterday)[2]), border = '1px solid #ddd'))
-                                       
-  output$recent_team_wins <- DT::renderDataTable(team_Wins_Yesterday2, rownames = FALSE,
-                                                 options = list(searching = FALSE,
-                                                                pageLength = 15, 
-                                                                lengthChange = FALSE, info = FALSE,
-                                                                paging = FALSE)) 
+  
+  output$recent_team_wins <- DT::renderDataTable(datatable(team_Wins_Yesterday2, rownames = FALSE,
+                                                           options = list(searching = FALSE,
+                                                                          pageLength = 15, 
+                                                                          lengthChange = FALSE, info = FALSE,
+                                                                          paging = FALSE)) %>%
+                                                   formatStyle(c(1:dim(team_Wins_Yesterday2)[2]), border = '1px solid #ddd'))
   output$top20_plot_output <- renderPlotly({
-    if (input$type == 'Regular Season') {
-      top20_plot(top_20pt_scorers) }
-    else {
-      top20_plot(top_20pt_scorers_p)
-    }
+      top20_plot(top_20pt_scorers) 
+  })
+  
+  selected_team_injury <- reactive({
+    injury_data %>%
+      filter(FullName == input$select_team)
   })
   
   output$team_plot_output <- renderPlot({
@@ -108,9 +106,19 @@ server <- function(input, output, session) {
   })
   
   
+  selected_team_ppg <- reactive({
+    team_ppg_scorers %>%
+      filter(FullName == input$select_team)
+  })
+
+  output$team_ppg_plot_output <- renderPlotly({
+    team_ppg_plot(selected_team_ppg()) 
+  })
+  
+  
   selected_team <- reactive({
     team_mov %>%
-      filter(Team == input$select_team)
+      filter(FullName == input$select_team)
   })
   
   selected_team_bubble <- reactive({
@@ -123,7 +131,7 @@ server <- function(input, output, session) {
   
   selected_team_injury <- reactive({
     injury_data %>%
-      filter(Team == input$select_team)
+      filter(FullName == input$select_team)
   })
   
   output$team_mov_output <- renderPlotly({
@@ -131,107 +139,67 @@ server <- function(input, output, session) {
     mov_plot(df)
   })
   
-  output$contract_plot_output <- renderPlotly({
-    value_plot(gameLogs_Two)
-  })
-  
   output$injury_table <- DT::renderDataTable(selected_team_injury(), rownames = FALSE,
                                                  options = list(searching = FALSE,
                                                                 pageLength = 15, 
                                                                 lengthChange = FALSE, info = FALSE,
                                                                 paging = FALSE))
-  # output$schedule_output <- renderPlotly({
-  #   
-  #   ggplotly(cc, tooltip = c('text'))
-  # })
-  
-  output$three_point_shooting_output <- renderPlotly({
-    
-    plot_bubble_shooting(threePointShooting_Bubble)
-  })
-  
-  selected_team_differential <- reactive({
-    playoffs_differentials %>%
-      filter(Team == input$select_team)
-  })
-  
-  output$differentials_output <- renderPlotly({
-    plot_ppg_differentials(selected_team_differential())
-  })
   
   selected_team_bans <- reactive({
-    bubble_playoff_wins %>%
-      filter(Team == input$select_team)
+    team_wins %>%
+      filter(FullName == input$select_team)
   })
   
-  selected_team_bubble_bans <- reactive({
-    bubble_regular_wins %>%
-      filter(Team == input$select_team)
+  selected_team_last_season <- reactive({
+    last_season_wins %>%
+      filter(FullName == input$select_team)
   })
   
-  output$playoff_wins <- renderValueBox({
-    playoffs_valuebox_function(selected_team_bans())
+  output$regular_wins <- renderValueBox({
+    regular_valuebox_function(selected_team_bans())
   })
   
-  output$bubble_wins <- renderValueBox({
-    bubble_valuebox_function(selected_team_bubble_bans())
+  output$last_season_wins <- renderValueBox({
+    last_season_valuebox_function(selected_team_last_season())
   })
   
-  # player_info <- reactive({
-  #   req(input$player_search != '')
-  #   available_players %>%
-  #     filter(str_detect(display_first_last, regex(input$player_search, ignore_case = TRUE)))
-  #   
-  #   
-  # })
-  # 
-  # observeEvent(input$player_search, {
-  #   choices <- player_info()$display_first_last
-  #   names(choices) <- choices
-  #   
-  #   updateSelectInput(session, 'select_player',
-  #                     choices = choices,
-  #                     selected = player_info()$name[1])
-  #   
-  # })
-  # 
-  # selected_player <- reactive({
-  #   req(nrow(player_info()) > 0) # tells the server to wait until we actually have data in artist_info()
-  #   player_info() %>% 
-  #     filter(display_first_last == input$select_player)
-  # })
-  # 
-  # observeEvent(input$select_player, {
-  #   
-  #   year_2 <- year_df %>%
-  #     filter(bb >= selected_player()$from_year & bb <= selected_player()$to_year)
-  #   
-  #   ccc <- unique(year_2$year_choices)
-  #   
-  #   updateSelectInput(session, 'select_year',
-  #                     choices = ccc,
-  #                     selected = ccc[1])
-  #   
-  #   
-  # })
-  # 
-  # observeEvent(input$generate_plot, {
-  #   req(input$select_player != '')
-  #   player_name <- input$select_player
-  #   season_year <- input$select_year
-  #   season_type_choice <- input$select_type
-  #   player_code <- get_player_code(player_name)
-  #   shot_data <- get_data(player_code, season_year, season_type_choice)
-  #   
-  #   output$hexagon_plot <- renderPlot({
-  #     
-  #     suppressWarnings(draw_hexagon(shot_data))
-  #   })
-  #   
-  # })
+  output$game_types_output <- renderPlotly({
+    game_types_plot(game_types)
+  })
+  
+  output$contract_value_output <- renderPlotly({
+    value_plot(contract_df)
+  })
+  
+  output$team_contract_value_output <- renderPlotly({
+    team_contract_value_plot(team_contract_value)
+  })
+  
+  output$bans_avg_pts <- renderValueBox({
+    valueBox(
+      value = main_bans$league_avg, HTML(paste0("League Average Points Scored <br> <br> ",
+                                                         main_bans$pct_change, "% difference from Last Season")),
+      icon = icon("caret-up"), color = "blue"
+    )
+  })
+  
+  output$bans_date <- renderValueBox({
+    gp_valuebox_function(main_bans)
+  })
+  
+  output$bans_homeroad <- renderValueBox({
+    valueBox(
+      value = main_bans$record, HTML(paste0("League Wide Home - Road Win Record <br> <br> ",  
+                                                       main_bans$`Win Percentage Home`, "% - ", main_bans$`Win Percentage Road`,
+                          '% Win Percentage Splits')),
+      icon = icon("chart-bar"), color = "blue"
+    )
+  })
+  
 }
 
 
 
 # Run the App
 shinyApp(ui, server)
+
