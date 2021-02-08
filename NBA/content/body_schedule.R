4dabc8df2b80a42a6d25550da11a0cbdceb12e70
jyablonski <jyablonski9@gmail.com>
2021-02-08 13:45:10 -0800

    Update body_schedule.R

--- NBA/content/body_schedule.R
+++ NBA/content/body_schedule.R
@@ -1,28 +1,30 @@
 body_schedule <- dashboardBody(
-  fluidRow(tags$a(href="https://www.espn.com/nba/schedule", "Click here for National TV Schedule")),
+  fluidRow(tags$a(href="https://www.espn.com/nba/schedule", "Click here for National TV Schedule"),
+           HTML(" | Moneyline Odds for today's games are in parentheses")),
   fluidRow(
     column(width = 12, DT::dataTableOutput("schedule_table")),
   ),
   fluidRow(
     box(
-      title = "Updated Strength of Schedule Analysis",
+      title = "Regular Season Game Type Analysis",
       status = "success",
       solidHeader = TRUE,
       collapsible = FALSE,
       width = 6,
-      plotlyOutput("schedule_output", height = "600px")
+      plotlyOutput("game_types_output", height = "600px")
     ),
     box(
-      column(
-        "The Graph on the left shows the 22 Teams invited to the NBA Bubble, ordered by the Average Rank of their 
-        opponents for the remaining games left in the Regular Season.",
-        tags$br(),
-        tags$br(),
-        "Teams in Blue are facing lesser competition than their Team Rank, while Teams in Red are facing harder competition.  
-        The Teams at the top have, on-paper, the easiest remaining schedule while the teams toward the bottom have a harder schedule.",
-        
-        width = 12
-        )
+      fluidRow(
+        column(4, 
+               selectInput('select_choice', "Select a Plot",
+                           choices = c('Strength of Schedule (as of Today)', 'Future Strength of Schedule'))
+        )),
+      title = "NBA Strength of Schedule Analysis",
+      status = "success",
+      solidHeader = TRUE,
+      collapsible = FALSE,
+      width = 6,
+      plotlyOutput("schedule_plot_output", height = "600px")
     )
   )
 )
