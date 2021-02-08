a944170e45f402fb25c327e73cb46eba7448b1b0
jyablonski <jyablonski9@gmail.com>
2021-02-08 13:45:49 -0800

    Update body_team_plots.R

--- NBA/content/body_team_plots.R
+++ NBA/content/body_team_plots.R
@@ -1,9 +1,21 @@
 body_team_plots <- dashboardBody(
   fluidRow(
-    box(
-      selectInput('select_team', h3("Select a Team"),
-                  choices = team_choices,
-  ))),
+    box(h4(paste0("Key Team Metrics as of ", today)),
+        fluidRow(
+          column(
+            box(
+              selectInput('select_team', h3("Select a Team"),
+                          choices = team_choices),
+             width = 3),
+            valueBoxOutput("regular_wins", width = 3),
+            valueBoxOutput("team_ratings_rank", width = 3),
+            valueBoxOutput("team_defensive_ratings_rank", width = 3),
+            width = 12,
+            style = "margin-left: -20px"
+          )
+        ),
+        div("Last updated: ", updated_date),
+        width = 12)),
   fluidRow(
     box(
       title = "Team Margin of Victory Plot",
@@ -13,7 +25,16 @@ body_team_plots <- dashboardBody(
       width = 6,
       plotlyOutput("team_mov_output", height = "600px")
     ),
-    column(width = 6, "Injury Table", DT::dataTableOutput("injury_table"))
+    box(
+      title = "Team PPG Plot",
+      status = "success",
+      solidHeader = TRUE,
+      collapsible = FALSE,
+      width = 6,
+      plotlyOutput("team_ppg_plot_output", height = "600px")
+    )),
+  fluidRow(
+    column(width = 6, h4("Injury Table"), DT::dataTableOutput("injury_table")),
+    column(width = 6, h4('Transactions Table'), DT::dataTableOutput("transactions_table"))
   )
 )
-
