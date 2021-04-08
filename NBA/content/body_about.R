3aa2f29544c754a9b620f3c33ebf3fc9b6d3d7bc
jyablonski <jyablonski9@gmail.com>
2021-04-08 15:15:21 -0700

    Update body_about.R

--- NBA/content/body_about.R
+++ NBA/content/body_about.R
@@ -1,29 +1,30 @@
-body_about <- dashboardBody(fluidRow(
+body_about <- dashboardBody(
   fluidRow(
-    column(
-      box(
-        title = div("About this project", style = "padding-left: 20px", class = "h2"),
-        column(
-          "This dashboard shows up to date information about the NBA Restart this Summer at the Orlando Bubble.",
-          tags$br(),
-          h3("Data"),
-          HTML("Gamelog data is collected via the nbastatR package in R, which scrapes from the NBA's Stats API. Injury & Schedule data
-               is web scraped from basketball-reference.com."),
-          tags$br(),
-          tags$br(),
-          HTML("The Dashboard's data is automatically updated whenever this application is loaded by a user."),
-          h3("Developer"),
-          "Jacob Yablonski | ",
-          tags$a(href = "https://www.linkedin.com/in/jacobyablonski/", "LinkedIn"), "|",
-          tags$a(href = "https://github.com/jyablonski", "Github"),
-          width = 12,
-          style = "padding-left: 20px; padding-right: 20px; padding-bottom: 40px; margin-top: -15px;"
+    fluidRow(
+      column(
+        box(
+          title = div("About this Project", style = "padding-left: 20px", class = "h2"),
+          column(
+            "This dashboard shows up to date information about the 2020-2021 NBA Season.",
+            tags$br(),
+            h3("Data"),
+            HTML("Game log data is collected via the nbastatR package in R, which scrapes from the NBA's Stats API. Injury, Schedule, and
+          Transaction data is web scraped from basketball-reference.com.  Gambling odds are taken from mybookie.ag for that day's games."),
+            tags$br(),
+            tags$br(),
+            HTML("The Dashboard's data is automatically updated whenever this application is loaded by a user."),
+            h3("Developer"),
+            "Jacob Yablonski | ",
+            tags$a(href = "https://www.linkedin.com/in/jacobyablonski/", "LinkedIn"), "|",
+            tags$a(href = "https://github.com/jyablonski", "Github"),
+            width = 12,
+            style = "padding-left: 20px; padding-right: 20px; padding-bottom: 40px; margin-top: -15px;"
+          ),
+          width = 6
         ),
-        width = 6
-      ),
-      width = 12,
-      style = "padding: 15px"
+        width = 12,
+        style = "padding: 15px"
+      )
     )
   )
-)
 )
